d729ce7804d88ffb002eeef0cc53efb124e1613e
Eugene Derevyanko <ederevyanko@cybervisiontech.com>
2017-03-14 19:56:10 +0200

    init commit

---
+++ src/main/java/com/symulakr/services/uploadfiles/storage/StorageException.java
@@ -0,0 +1,12 @@
+package com.symulakr.services.uploadfiles.storage;
+
+public class StorageException extends RuntimeException {
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/com/symulakr/services/uploadfiles/storage/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package com.symulakr.services.uploadfiles.storage;
+
+public class StorageFileNotFoundException extends StorageException {
+
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
